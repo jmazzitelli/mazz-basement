ade64a2666a3edbb182d3e2faee2eb25aa0734d1
John Mazz <mazz@redhat.com>
2021-10-20 20:36:46 -0400

    move to latest ocp4-setup scripts and 4.9

--- server-scripts/INSTALL_OPENSHIFT/env.sh
+++ server-scripts/INSTALL_OPENSHIFT/env.sh
@@ -2,11 +2,11 @@
 
 MASTERS="3"
 WORKERS="0"
-OPENSHIFT_VERSION="4.7.20"
+OPENSHIFT_VERSION="4.9.0"
 
 SCRIPT_DIR="${HOME}/source/ocp4_setup_upi_kvm"
 SCRIPT_EXE="${SCRIPT_DIR}/ocp4_setup_upi_kvm.sh"
 
 INSTALL_CMD="${SCRIPT_EXE} -y -m ${MASTERS} -w ${WORKERS} -O ${OPENSHIFT_VERSION}"
 
-OCP4_SETUP_DIR="/root/ocp4_setup_ocp4"
+OCP4_SETUP_DIR="/root/ocp4_cluster_ocp4"
