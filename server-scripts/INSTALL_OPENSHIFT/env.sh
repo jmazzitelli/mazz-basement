8f78442722a0109b343dbe0159773617f333375d
John Mazz <mazz@redhat.com>
2022-10-18 08:25:35 -0400

    bump to 4.11.9

--- server-scripts/INSTALL_OPENSHIFT/env.sh
+++ server-scripts/INSTALL_OPENSHIFT/env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Versions that are currently supported:
+# - https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/
+# - https://mirror.openshift.com/pub/openshift-v4/x86_64/dependencies/rhcos/4.11/
+
 MASTERS="3"
 WORKERS="0"
-OPENSHIFT_VERSION="4.10.13"
+OPENSHIFT_VERSION="4.11.9"
 
 SCRIPT_DIR="${HOME}/source/ocp4_setup_upi_kvm"
 SCRIPT_EXE="${SCRIPT_DIR}/ocp4_setup_upi_kvm.sh"
