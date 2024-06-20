5320f2385c834147f4f9b5b967cb10c5bd843258
John Mazz <mazz@redhat.com>
2024-06-20 06:58:13 -0400

    Ignore errors on crc destroy

--- server-scripts/INSTALL_OPENSHIFT/crc-destroy.sh
+++ server-scripts/INSTALL_OPENSHIFT/crc-destroy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo "====Pulling the latest Kiali source code"
 cd ${HOME}/source/kiali
 git pull
 
 echo "====Destroying CRC"
-./hack/crc-openshift.sh delete
+if ! ./hack/crc-openshift.sh delete; then
+  echo "Attempt to delete failed - CRC was likely not running."
+  exit 0
+fi
